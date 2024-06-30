12b240fe0dceceea9ac0c7e26990be4ba301729b
maskopav <maskopav@fel.cvut.cz>
2024-06-30 23:16:48 +0200

    Loading data updated, entering sampling frequency when loading csv or txt file

--- interfaces/sigInspectDataCsv.m
+++ interfaces/sigInspectDataCsv.m
@@ -9,30 +9,61 @@ classdef sigInspectDataCsv < sigInspectDataInterface
     end
     methods
         % constructor - just store the path, set settings
-        function obj=sigInspectDataCsv(dirPath)
+        function obj=sigInspectDataCsv(dirPath, varargin)
             obj.dirPath = dirPath;
-            obj.settings.SAMPLING_FREQ=6000; % 6kHz sampling rate
-            obj.settings.PLOT_STEP=1.5;      % distance between channels on the y-axis
-            obj.settings.ARTIFACT_TYPES={'Type A','Type B','Unsure'};            
+            %obj.settings.PLOT_STEP=1.5;      % distance between channels on the y-axis
+            obj.settings.ARTIFACT_TYPES={'Type A','Type B','Unsure'};
+
+            samplingFreq = [];
+            % Check if the second argument is provided and it is numeric
+            if ~isempty(varargin) && isnumeric(varargin{1})
+                samplingFreq = varargin{1};
+            end
+
+            % If sampling frequency is still empty, prompt the user
+            if isempty(samplingFreq)
+                % If the second argument is not provided or not numeric, prompt the user
+                prompt = {'Enter sampling frequency:'};
+                dlgtitle = 'Input';
+                dims = [1 35];
+                definput = {'12000'}; % Default value
+                samplingFreqInput = inputdlg(prompt, dlgtitle, dims, definput);
+                if isempty(samplingFreqInput)
+                    disp('No sampling frequency provided. Using default 12000 Hz.');
+                    samplingFreq = 12000; % Default sampling frequency
+                else
+                    samplingFreq = str2double(samplingFreqInput{1}); % Convert from cell to double
+                end
+            end
+            % Set the sampling frequency
+            obj.settings.SAMPLING_FREQ = samplingFreq;
         end        
         % return list of signal ids - load all csv files from a directory,
         % use filenames as signalIds
         function signalIds = getSignalIds(obj)
-            if(isempty(obj.dirPath) || ~exist(obj.dirPath,'dir'))
-                error('Directory %s empty or does not exist',obj.dirPath)
+            if exist(obj.dirPath, 'file') == 2 % If it's a file
+                [~, name, ext] = fileparts(obj.dirPath);
+                signalIds = {[name ext]}; % Return the file name as the only signalId
+            elseif exist(obj.dirPath, 'dir') == 7 % If it's a directory
+                lst = dir([obj.dirPath '/*.csv']);
+                signalIds = {lst(:).name}';
+            else
+                error('Path %s does not exist', obj.dirPath);
             end
-            lst=dir([obj.dirPath '/*.csv']);
-            signalIds = {lst(:).name}';
         end
         
         % read signals based on signalId (=filename)
-        function [signals chInfo]= getSignalsById(obj,signalId)
-            filePath = [obj.dirPath '/' signalId];
-            if(~exist(filePath,'file'))
-                error('File %s does not exist',filePath)
+        function [signals, chInfo] = getSignalsById(obj, signalId)
+            if exist(obj.dirPath, 'file') == 2 % If dirPath is actually a file path
+                filePath = obj.dirPath; % Use it directly
+            else
+                filePath = fullfile(obj.dirPath, signalId); % Construct the file path
             end
-            chInfo='';
-            signals=csvread(filePath)'; % load + transpose to have signals in rows
+            if ~exist(filePath, 'file')
+                error('File %s does not exist', filePath);
+            end
+            chInfo = '';
+            signals = readmatrix(filePath)'; % Load and transpose to have signals in rows
         end
     end
 end
